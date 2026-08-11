bb9447f34dd96543aee62f98433e1fc7da827a9b
Kenichi Wakabayashi <kenichi@crssrds.jp>
2026-08-11 17:46:12 +0900

    並び順変更、画像差し替え

--- src/data/staff.ts
+++ src/data/staff.ts
@@ -20,7 +20,8 @@ export const STAFF_NO_IMAGE = "/images/staff/staff_noimage.webp";
 // NOTE: 一部はモック確認用のダミー名です。実データに合わせて調整してください。
 export const staff: Staff[] = [
   { name: "天沼倫太郎", avatar: "/images/staff/amanuma.jpg", url: "https://coderdojo-takizawa.doorkeeper.jp/" },
-  { name: "若林健一", avatar: "/images/staff/kwaka1208.png", url: "https://crssrds.jp/" },
+  { name: "柳原央子" },
+  { name: "日脇隆弘" },
   { name: "細谷崇", avatar: "/images/staff/hosoya.jpeg", url: "https://ht79.info/" },
   { name: "石橋優希", avatar: "/images/staff/ishibashi.jpg", url: "https://www.dropin8.jp/owner" },
   { name: "井上一", avatar: "/images/staff/inoue.png", url: "https://xn--coderdojo-ef6q138l.jp/" },
@@ -29,9 +30,8 @@ export const staff: Staff[] = [
   { name: "Jun1", avatar: "/images/staff/Jun1.jpg", url: "https://coderdojokibi.connpass.com/" },
   { name: "渕崎愛", avatar: "/images/staff/shinozaki.jpg", url: "https://deradesign.jp/" },
   { name: "高井和之", avatar: "/images/staff/takai.jpg", url: "https://coderdojo-koga.hyasynth.com/" },
-  { name: "日脇隆弘" },
-  { name: "柳原央子" },
   { name: "土居安佳里", avatar: "/images/staff/doi.jpg", url: "https://akarihonokani.com/" },
   { name: "長嶺建市", avatar: "/images/staff/nagamine.jpg", url: "https://www.sstn.jp/" },
   { name: "井川健一", avatar: "/images/staff/igawa.jpg", url: "https://coderdojo-mito.com/" },
+  { name: "若林健一", avatar: "/images/staff/kwaka1208.png", url: "https://crssrds.jp/" },
 ];
